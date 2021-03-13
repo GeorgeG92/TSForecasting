17b32fd22ada2315a911d1f0b4c57997f9b51e54
GeorgeG92 <ggousios1992@gmail.com>
2021-03-13 16:09:05 +0100

    minor changes, config sanity checks and hyperparameter storing

--- src/SARIMAX_model.py
+++ src/SARIMAX_model.py
@@ -1,5 +1,7 @@
 import numpy as np
 import warnings
+import itertools
+import time
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 import os
@@ -16,6 +18,8 @@ from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.stattools import adfuller
 from sklearn.metrics import mean_absolute_error
 from statsmodels.tsa.statespace.sarimax import SARIMAXResults
+from joblib import Parallel
+from joblib import delayed
 import logging
 
 logger = logging.getLogger(__file__)
@@ -30,6 +34,9 @@ class SARIMAXModel():
         self.exploreParams = config['Forecasting']['SARIMAX']['GridSearchCV']
         self.exportpath = os.path.join(args.exportpath, 'SARIMAX')
         self.modelpath = os.path.join(args.modelpath, 'SARIMAX_best_params.p')
+        self.useoptim = args.useoptimalparams
+        if self.useoptim:
+            self.bestparams = config['Optimal SARIMAX Parameters:']
         if 'Optimal SARIMAX Parameters' in config:
             self.optimExists = True
             self.bestParams = config['Optimal SARIMAX Parameters']
@@ -76,7 +83,7 @@ class SARIMAXModel():
         plt.ylabel('PACF')
         plt.savefig(os.path.join(self.exportpath, 'ACF_PACF_Plot.jpeg'))
 
-    def score_sarimax(train, exog_train, params):
+    def score_sarimax(self, train, exog_train, params):
         """ Trains a SARIMAX model using given parameters
         Args:
             train: the data to train to
@@ -107,7 +114,7 @@ class SARIMAXModel():
         logger.info("\tStarting GridSearch for best hyperparameters, evaluating {num} models".format(num=len(pdq)))
         logger.info("\t\tEstimated Time: {num} mminutes".format(num=len(pdq)*0.4))
         executor = Parallel(n_jobs=-1, backend='threading')
-        tasks = (delayed(score_sarimax)(train, exog_train, cfg) for cfg in pdq)
+        tasks = (delayed(self.score_sarimax)(train, exog_train, cfg) for cfg in pdq)
         scores = executor(tasks)
         # remove empty results
         scores = [r for r in scores if r[1] != None]
@@ -128,11 +135,10 @@ class SARIMAXModel():
         exog_test = exog[-(self.stepsOut):]
         return train, test, exog_train, exog_test
 
-    def train_predict_sarimax(self, df, params=None):
+    def train_predict_sarimax(self, df):
         """ Trains a SARIMAX model on the train data and runs inference on the test split
         Args:
             df: the dataframe containing the data
-            params: optional argument containing the best discovered parameters for the model
         """
 
         # Log Transform to battle Heteroscedasticity
@@ -140,27 +146,34 @@ class SARIMAXModel():
         # Train/Test Split
         train, test, exog_train, exog_test = self.train_test_split(df, data)
 
-        if os.path.exists(self.modelpath):      # if model exists then load, otherwise train on optimal, otherwise GridSearchCV
+        if not self.train:
             logger.info("\tLoading Model from Disk...")
             result = SARIMAXResults.load(self.modelpath)
-        elif params:
+        elif self.useoptim:
             logger.info("\tTraining using optimal parameters")
-            p,d,q,P,D,Q,s = params['p'], params['d'], params['q'], params['P'], params['D'], params['Q'], params['s']
+            p,d,q,P,D,Q,s = self.bestparams['p'], self.bestparams['d'], self.bestparams['q'], self.bestparams['P'], self.bestparams['D'], self.bestparams['Q'], self.bestparams['s']
             model = SARIMAX(train, order=(p,d,q),seasonal_order=(P,D,Q,s), exog=exog_train)
             result = model.fit(disp=False)
             if not os.path.exists(self.modelpath):
                 result.save(self.modelpath)
         else:
+            logger.info("\tGridSearchCV SARIMAX to detect optimal parameters")
+            start = time.time()
             bp = self.grid_search_sarimax(train, exog_train)
+            end = time.time()
+            logger.info("\tGridSearchCV finished in {time} minutes".format(time=round((end-start)/60, 3)))
+            # Retrain SARIMAX using best found hyperparameters
             p,d,q,P,D,Q,s = bp[0], bp[1], bp[2], bp[3], bp[4], bp[5], bp[6]
             model = SARIMAX(train, order=(p,d,q),seasonal_order=(P,D,Q,s), exog=exog_train)
             result = model.fit(disp=False)
             result.save(self.modelpath)
-            # Save optimal parameters to config
-            # BestParamsDict = {}
-            # BestParamsDict['Optimal Parameters'] = bestParameters
-            # with open('config.yaml', 'a') as outfile:
-            #     yaml.dump(BestParamsDict, outfile, default_flow_style=False)
+
+            #Save optimal parameters to opt config
+            best_params_dict = {'p':p,'d':d, 'q':q, 'P':P, 'D':D, 'Q':Q, 's':s}
+            with open(os.path.join(args.configpath, args.configoptname)) as outfile:
+                configOpt = yaml.load(file, Loader=yaml.FullLoader)
+                configOpt['Optimal SARIMAX Parameters'] = best_params_dict
+                yaml.dump(configOpt, outfile, default_flow_style=False)
 
         #print(result.summary())
         trainpreds = result.fittedvalues
@@ -211,14 +224,4 @@ class SARIMAXModel():
             df: the dataframe containing the time series data
         """
         logger.info("SARIMAX Modeling")
-        self.generate_plots(self.df)
-        if self.train:
-            if self.optimExists:
-                self.train_predict_sarimax(self.df, self.bestParams)
-            else:
-                self.train_predict_sarimax(self.df)
-        else:
-            if self.optimExists:
-                self.train_predict_sarimax(self.df, self.bestParams)
-            else:
-                self.train_predict_sarimax(self.df)
+        self.train_predict_sarimax(self.df)
--- src/arg_parser.py
+++ src/arg_parser.py
@@ -2,82 +2,104 @@ import os
 import sys
 import yaml
 import argparse
+import logging
+
+logger = logging.getLogger(__file__)
 
 
 def arg_parser(argv):
-    """This function implements an argument parser
-    Args:
-        argv: Ιnput arguments from the command line.
-    Returns:
-        An args object that contains parsed arguments.
-    """
-    parser = argparse.ArgumentParser(description='Flight Bookings Forecasting')
+	"""This function implements an argument parser
+	Args:
+		argv: Ιnput arguments from the command line.
+	Returns:
+		An args object that contains parsed arguments.
+	"""
+	parser = argparse.ArgumentParser(description='Flight Bookings Forecasting')
 
-    parser.add_argument('-l', '--logging', dest='logging_level', type=str.upper,
-                        default='info', help='set logging level',
-                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'])
+	parser.add_argument('-l', '--logging', dest='logging_level', type=str.upper,
+						default='info', help='set logging level',
+						choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'])
 
-    parser.add_argument('-m', '--method', default='SARIMAX',
-                        help='Set forecasting method', choices=['LSTM', 'SARIMAX'])
+	parser.add_argument('-m', '--method', default='SARIMAX', dest='method',
+						help='Set forecasting method', choices=['LSTM', 'SARIMAX'])
 
-    parser.add_argument('-t','--train', action='store_true', default=False,       
-                     help='Retrain model with GridSearchCV')
+	parser.add_argument('-t','--train', action='store_true', default=True,       
+					 help='Retrain model with GridSearchCV')
 
-    parser.add_argument('-c','--cluster', action='store_true', default=False,
-                     help='Cluster the data on Lima map')
+	parser.add_argument('-c','--cluster', action='store_true', default=False,
+					 help='Cluster the data on Lima map')
 
-    parser.add_argument('-e','--explore', action='store_true', default=False,   
-                        dest='explore',
-                        help='Explore K-means Clustering and use the elbow rule to define optimal K')
+	parser.add_argument('-e','--explore', action='store_true', default=False,   
+						dest='explore',
+						help='Explore K-means Clustering and use the elbow rule to define optimal K')
 
-    parser.add_argument('-i', '--impute', action='store_true', default=False,
-                        help='Select imputation fast (KNN) or slow (MICE) over dropping')
+	parser.add_argument('-i', '--impute', action='store_true', default=False,
+						help='Select imputation fast (KNN) or slow (MICE) over dropping')
 
-    parser.add_argument('--inputemethod', type=str.upper, choices=['KNN', 'MICE'],
-                        default='KNN', help='Imputation method choice')
+	parser.add_argument('--inputemethod', type=str.upper, choices=['KNN', 'MICE'],
+						default='KNN', help='Imputation method choice')
 
-    parser.add_argument('-d', '--datapath', dest='datapath',
-                        default=os.path.join('..','data'),
-                        help='The path to the data directory')
-    
-    parser.add_argument('-f', '--filename', dest='filename',
-                        default='routes.csv',
-                        help='The name of the data file')
+	parser.add_argument('-d', '--datapath', dest='datapath',
+						default=os.path.join('..','data'),
+						help='The path to the data directory')
+	
+	parser.add_argument('-f', '--filename', dest='filename',
+						default='routes.csv',
+						help='The name of the data file')
 
-    parser.add_argument('--modelpath', dest='modelpath',
-                        default=os.path.join('..', 'model'),
-                        help='The path to the model directory')
+	parser.add_argument('--modelpath', dest='modelpath',
+						default=os.path.join('..', 'model'),
+						help='The path to the model directory')
 
-    parser.add_argument('--configpath', dest='configpath',
-                        default=os.path.join('..','config'),
-                        help='The path to the config directory')
-    
-    parser.add_argument('--configname', dest='configname',
-                        default='config.yaml',
-                        help='The name of the config file')
+	parser.add_argument('--configpath', dest='configpath',
+						default=os.path.join('..','config'),
+						help='The path to the config directory')
+	
+	parser.add_argument('--configname', dest='configname',
+						default='config.yaml',
+						help='The name of the config file')
 
-    parser.add_argument('--exportpath', dest='exportpath',
-                        default=os.path.join('..','output'),
-                        help='The path to the output directory')
+	parser.add_argument('--configoptname', dest='configoptname',
+						default='optimal_parameters.yaml',
+						help='The name of the second config file')
 
-    parser.add_argument('--weatherfile', dest='weatherfile',
-                        default='lima_2015_weatherdata.csv',
-                        help='The name of the config file')
+	parser.add_argument('--exportpath', dest='exportpath',
+						default=os.path.join('..','output'),
+						help='The path to the output directory')
+
+	parser.add_argument('--weatherfile', dest='weatherfile',
+						default='lima_2015_weatherdata.csv',
+						help='The name of the config file')
 
 
-    args = parser.parse_args(argv[1:])                                           # exclude filename
-    if args.explore and not args.cluster:
-        print("Argument error: --explore argument requires --cluster")
-        sys.exit(1)
-    if not os.path.exists(os.path.join(args.modelpath, args.method)):
-        print("Creating Model path")
-        os.makedirs(os.path.join(args.modelpath, args.method))
-    args.modelpath = os.path.join(args.modelpath, args.method)
-    if not os.path.exists(args.exportpath):
-        print("Creating exportpath")
-        os.makedirs(os.path.join(args.exportpath))
-        os.mkdir(os.path.join(args.exportpath, 'Cleaning'))
-        os.mkdir(os.path.join(args.exportpath, 'Clustering'))
-        os.mkdir(os.path.join(args.exportpath, 'TS_Decomposition'))
-    assert os.path.exists(os.path.join(args.datapath, args.filename)), "Data file {file} does not exist".format(file=os.path.join(args.datapath, args.filename))
-    return args
+	args = parser.parse_args(argv[1:])                                           # exclude filename
+	if args.train:
+		if args.method=='LSTM' and not os.path.exists(args.modelpath):
+			logger.warning("Train is set to False but model doesn't exist at {p}".format(p=os.path.exists(os.path.join(rgs.modelpath, 'SARIMAX_best_params.p'))))
+			logger.warning("Setting train to True")
+			args.train=True
+		else:
+			logger.warning("Train is set to False but model doesn't exist at {p}".format(p=os.path.exists(os.path.join(args.modelpath, 'SARIMAX_best_params.p'))))
+			logger.warning("Setting train to True")
+			#assert os.path.exists(args.modelpath, 'SARIMAX_best_params.p'), "Train is set to False but model doesn't exist at {p}".format(p=os.path.exists(args.modelpath, 'SARIMAX_best_params.p'))
+			args.train = True
+
+	if args.explore and not args.cluster:
+		logger.error("Argument error: --explore argument requires --cluster")
+		sys.exit(1)
+	if not os.path.exists(os.path.join(args.modelpath, args.method)):
+		logger.info("Creating Model path")
+		os.makedirs(os.path.join(args.modelpath, args.method))
+	args.modelpath = os.path.join(args.modelpath, args.method)
+	if not os.path.exists(args.exportpath):
+		logger.info("Creating exportpath")
+		os.makedirs(os.path.join(args.exportpath))
+		os.mkdir(os.path.join(args.exportpath, 'Cleaning'))
+		os.mkdir(os.path.join(args.exportpath, 'Clustering'))
+		os.mkdir(os.path.join(args.exportpath, 'TS_Decomposition'))
+	assert os.path.exists(os.path.join(args.datapath, args.filename)), "Data file {file} does not exist".format(file=os.path.join(args.datapath, args.filename))
+	if not os.path.exists(os.path.join(args.configpath, args.configoptname)):                # 
+		args.useoptimalparams = False
+	else:
+		args.useoptimalparams = True
+	return args
